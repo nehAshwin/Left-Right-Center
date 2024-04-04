c63de460f5f9edaae63481c29f78fe3b9f3f0af9
nehAshwin <107515350+nehAshwin@users.noreply.github.com>
2024-04-03 23:39:30 -0700

    Update names.h

--- names.h
+++ names.h
@@ -1,1 +1,11 @@
+/*
+* File:     names.h
+* Purpose:  Provide player names for Left Right Center
+*/
+
+const char *player_name[] = { "Ada Lovelace", "Margaret Hamilton", "Katherine Johnson",
+    "Joy Buolamwini", "Grace Hopper", "Adele Goldberg", "Annie Easley", "Jeannette Wing",
+    "Mary Kenneth Keller", "Megan Smith", "Radia Perlman" };
+
+#define MAX_PLAYERS ((int) (sizeof(player_name) / sizeof(player_name[0])))
 
